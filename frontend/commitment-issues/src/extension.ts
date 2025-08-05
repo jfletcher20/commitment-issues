28dba2f3fd6ea3313b5f0d27b8e8841d48c8c7c2
Joshua Fletcher <rovinjosh@gmail.com>
2025-08-05 18:32:26 +0200

    Add configuration parameters to package.json.
    Configuration parameters appear in the settings view of VSCode along with the settings for other extensions. Using markdownDescription allows for better quality descriptions of config items.

--- frontend/commitment-issues/src/extension.ts
+++ frontend/commitment-issues/src/extension.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Commitment Issues extension is active. Thank you for installing our extension! -Noa and Joshua.');
+	console.log('Commitment Issues extension active. Thank you for installing our extension! -Noa and Joshua.');
 }
 
 export function deactivate() {}
