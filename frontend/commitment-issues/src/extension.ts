af9115924326a48bb4ae383877b36bf7b486bd71
Joshua Fletcher <rovinjosh@gmail.com>
2025-08-05 19:13:11 +0200

    Add welcome message with button for config.

--- frontend/commitment-issues/src/extension.ts
+++ frontend/commitment-issues/src/extension.ts
@@ -2,6 +2,12 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Commitment Issues extension active. Thank you for installing our extension! -Noa and Joshua.');
+	context.subscriptions.push(
+		vscode.commands.registerCommand('commitment-issues.openConfig', () => {
+			vscode.commands.executeCommand('workbench.action.openSettings', 'Commitment Issues config');
+		})
+	);
+
 }
 
-export function deactivate() {}
+export function deactivate() { }
