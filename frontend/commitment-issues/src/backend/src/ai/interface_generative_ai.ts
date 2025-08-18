4c0494170c1f7c43fbb85092d01b4a43d48bedcd
nmidzic20 <nmidzic@yahoo.com>
2025-08-18 16:46:30 +0200

    Rework folder structure

--- frontend/commitment-issues/src/backend/src/ai/interface_generative_ai.ts
+++
@@ -1,7 +0,0 @@
-import { Commit } from "../models/commit";
-
-export interface GenerativeAI {
-  test(): Promise<string>;
-  getContents(): string;
-  analyzeCommits(commits: Commit[]): Promise<string>;
-}
